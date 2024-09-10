4c667f83cc5676f4361cb4bf653694d07d623da3
AmitosForever <amit.ru@campus.technion.ac.il>
2024-09-10 17:56:09 +0300

    added discard indicator

--- kalman_filter.py
+++ kalman_filter.py
@@ -26,6 +26,7 @@ class KF:
         self.R = R
         self.x = x_0
         self.P = P_0
+        self.discard = False
     
     def predict(self):
 
@@ -52,10 +53,13 @@ class KF:
             s_inv = 1 / S
         else:
             s_inv = np.linalg.inv(S)
+
         # Kalman gain
         K = np.dot(np.dot(self.P, self.H.T), s_inv) # Named L in the lecture notes
+
         # Measurement residual (innovation)
         y = z - np.dot(self.H, self.x)
+
         # Debugging 
         if debug:
             print("--- H dot x ---")
@@ -70,11 +74,16 @@ class KF:
             print(np.dot(K, y))
             print("--- sigma calculation ---")
             print(np.sqrt((np.dot(np.dot(y.T, s_inv), y).astype(float)) / 3))
+
         # Likely estimation of mesurments. If it is larger then 3*sigma of the measurment sigma then discard the measurment
         if np.sqrt((np.dot(np.dot(y.T, s_inv), y).astype(float)) / 3) > 3:
             self.x = self.x
             self.P = self.P 
+            self.discard = True
             return 
+        
+        self.discard = False # If the estimation is likely
+
         # Updated state estimate
         if y.ndim == 1 and y.shape[0] == 1:
             a = K * y
@@ -82,6 +91,7 @@ class KF:
             self.x = self.x + a
         else:
             self.x = self.x + np.dot(K, y)
+        
         # Updated covariance estimate
         self.P = self.P - np.dot(np.dot(K, self.H), self.P)
 
@@ -94,7 +104,7 @@ class KF:
             np.ndarray: Current state estimate.
         """
         #print(self.x.shape)
-        return self.x  
+        return self.x, self.discard
     
     def update_system(self, F, G):
 
@@ -139,13 +149,17 @@ def kf_3d(data, t_diff, kalman):
 
     time_velocity : np.ndarray
         Predicted velocities after the Kalman filter time update.
+
+    discard_ind : np.ndarray
+        Indicator for when data is discarded as a result of GPS interference
     """
     
-    # Initialize arrays to store measurement and time updates and velocities
+    # Initialize arrays to store measurement and time updates, velocities and discard indicator
     meas_update = np.zeros_like(data[:, :3])
     time_update = np.zeros_like(data[:, :3])
     meas_velocity = np.zeros_like(data[:, :3])
     time_velocity = np.zeros_like(data[:, :3])
+    discard_ind = np.zeros_like(data[:, 0])
 
     # Iterate through the data points, except the last one
     for i in range(len(data[:, 0]) - 1):
@@ -173,12 +187,14 @@ def kf_3d(data, t_diff, kalman):
         
         # Perform the measurement update using the current data point
         kalman.update(data[i, :3].reshape(3, 1))
-        meas_update[i, :] = kalman.current_state()[:3, :].reshape(3)
-        meas_velocity[i, :] = kalman.current_state()[3:, :].reshape(3)
+        meas, discard_ind[i] = kalman.current_state()
+        meas_update[i, :] = meas[:3, :].reshape(3)
+        meas_velocity[i, :] = meas[3:, :].reshape(3)
 
         # Predict the next state and store the time update
         kalman.predict()
-        time_update[i, :] = kalman.current_state()[:3, :].reshape(3)
-        time_velocity[i, :] = kalman.current_state()[3:, :].reshape(3)
+        time, _ = kalman.current_state()
+        time_update[i, :] = time[:3, :].reshape(3)
+        time_velocity[i, :] = time[3:, :].reshape(3)
 
-    return meas_update, time_update, meas_velocity, time_velocity
+    return meas_update, time_update, meas_velocity, time_velocity, discard_ind
